f69b9aa18d57bb9de4040d7891a9cc65ec3e4159
rollexxx23 <arinchatterjee00@gmail.com>
2022-12-16 13:00:56 +0530

    added database connection

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/rollexxx23/crud-app
 
 go 1.18
+
+require github.com/lib/pq v1.10.7 // indirect
--- server/main.go
+++ server/main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/lib/pq"
 	"github.com/rollexxx23/crud-app/routes"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome Welcome Welcome...")
+	addr := flag.String("addr", ":6969", "HTTP network address")
+	flag.Parse()
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	fileServer := http.FileServer(http.Dir("../client/static/"))
 	mux := http.NewServeMux() // creates the server
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/snippet/get-list", routes.ViewSnippetById)
 	mux.HandleFunc("/snippet/create", routes.CreateSnippet)
 	mux.HandleFunc("/", routes.Home) // routing handler
-	log.Println("Starting server on :6969")
-	err := http.ListenAndServe(":6969", mux)
+	log.Printf("Starting server on %s", *addr)
+
+	err = http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 
+}
+
+func openDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "postgres://aamjujdd:OeHi-7z8o0ptdAs69NJqVRH14ms-zb6R@tiny.db.elephantsql.com/aamjujdd")
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	fmt.Println("Connected to DB...")
+	return db, nil
 }
